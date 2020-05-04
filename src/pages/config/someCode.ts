a2087f63af976fec9839a3280cfe959e4ca2b90d
changlei <769518355@qq.com>
2020-05-04 22:51:42 +0800

    no message

--- src/pages/config/someCode.ts
+++
@@ -1,7 +0,0 @@
-// 配置一些需要做特殊处理的问题代码和答案代码 希望不要在store里面直接写问题和答案的代码
-
-// 第8个问题的id：您患有或被怀疑患有的是下列哪些疾病
-export const question8id = 'cec4c6eb-685a-4cc3-b741-fb63c91ddeb3';
-
-// 第8个问题的最后一个答案的id：未患有或被怀疑患有上述相关疾病
-export const question8lastAnswerId = 'ef5b2f2d-a0cd-4630-8ccd-ece05cee29c4';
